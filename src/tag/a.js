21cbf1a4c3518348fc1cf81d1861053d9d00d3bc
chkui <chenkui@chkui.com>
2017-06-19 16:23:26 +0800

    add src path

---
+++ src/tag/a.js
@@ -0,0 +1,46 @@
+/**
+ * Created by chkui on 2017/5/27.
+ */
+
+import React from 'react'
+import {withRouter} from '../../router'
+
+/**
+ *  内置A标签。
+ *  1）标签提供服务器跳转和本地跳转2种模式。通过server参数配置。
+ *  @param {object} props{
+ *      {string} href:要跳转的路径
+ *      {boolean} server:是否经过服务器跳转，默认为false。
+ *      {object} style: 样式
+  *     {string} className: css样式
+ *  }
+ */
+const A = withRouter(class extends React.Component {
+    constructor(...props) {
+        super(...props);
+        const params = this.props;
+        this.hrefHandle = this.hrefHandle.bind(this);
+        //设定参数
+        this.params = {};
+        params.style && (this.params.style = params.style);
+        params.className && (this.params.className = params.className);
+        if (params.server) {
+            this.params.href = params.href
+        } else {
+            this.params.onClick = this.hrefHandle;
+        }
+    }
+
+    hrefHandle() {
+        const {href, history} = this.props;
+        href && history.push(href);
+    }
+
+    render() {
+        return (
+            <a {...this.params}>{this.props.children}</a>
+        )
+    }
+});
+
+export default A
---
+++ src/tag/dynaIcon.js
@@ -0,0 +1,32 @@
+/**
+ * Created by chkui on 2017/6/9.
+ */
+
+import React from 'react'
+import Icon from './icon'
+
+/**
+ * 提供激活支持的Icon组件
+ * 1）标签组件需要绑定资源路径使用，资源路径的配置文件默认在res/index中。每增加一个图片，都需要增加一个资源引用。
+ * 2）src参数传递的是资源标记，例如资源项 img={logo:"base64:adf"},此时传入的src="logo"。
+ * 3）标签组件的作用1：将资源文件和源代码隔离开，便于分部加载。
+ * 4）支持所有img标签原有属性
+ * @param {object} props {
+ *  {object} style 样式
+ *  {string} className css样式
+ *  {string} alt 图标别名
+ *  {string} src 图片标识
+ *  {string} actSrc 激活图片标识
+ *  {boolean} act 是否激活标记true标识激活,需要动态传入
+ * }
+ * @constructor
+ */
+const DynaIcon = (props) => {
+    const params = Object.assign({}, props);
+    params.act && (params.src = params.actSrc)
+    delete params.actSrc
+    delete params.act
+    return (<Icon {...params}/>)
+}
+
+export default DynaIcon
