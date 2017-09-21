9faf689f238bc057f933016b4b0796547cb859e7
chenkui <chenkui@chkui.com>
2017-09-21 17:37:49 +0800

    fix Img bug

--- src/lib/tag/img.js
+++ src/lib/tag/img.js
@@ -3,13 +3,12 @@
  * Created by chkui on 2017/5/27.
  */
 import React from 'react'
-import OriginImg from 'react-scroll-over-img'
 /**
  * 图片组件,参数说明见：https://github.com/palmg/react-scroll-over-img#readme
  * @param {string} props 可以设置任意图片属性
  * @constructor
  */
-const Img = props =><OriginImg {...props}/>;
+const Img = props =><img {...props}/>;
 export default Img
 
 
