da951202f90d32613f9f1321ee2a436b23ba0ad2
chenkui <chenkui@chkui.com>
2017-10-19 17:48:09 +0800

    create testunit to check store dispatch

--- test/flux.test.js
+++ test/flux.test.js
@@ -2,10 +2,10 @@
  * Created by chkui on 2017/6/20.
  */
 
-import {buildStore, getStore} from '../flux'
+import {buildStore, getStore, dispatch, subscribe} from '../flux'
 
-test('test flux ', ()=>{
-    const reducer1 = (state = {test1:'test1'}, action) => {
+test('test flux ', () => {
+    const reducer1 = (state = {test1: 'test1'}, action) => {
         switch (action.type) {
             case 'test1':
                 return action.data;
@@ -13,6 +13,29 @@ test('test flux ', ()=>{
                 return state;
         }
     };
-    buildStore({reducer1:reducer1})
-    expect(getStore().getState()).toEqual({reducer1:{test1:'test1'}});
+    buildStore({reducer1: reducer1})
+    expect(getStore().getState()).toEqual({reducer1: {test1: 'test1'}});
+})
+
+test('test action flux ', done => {
+    const reducer1 = (state = {test1: 'test1'}, action) => {
+            switch (action.type) {
+                case 'test1':
+                    return action.data;
+                default :
+                    return state;
+            }
+        },
+        action1 = () => {
+            return {
+                type:'test1',
+                data:{test1: 'dispatch data'}
+            }
+        }
+    buildStore({reducer1: reducer1})
+    subscribe(()=>{
+        expect(getStore().getState()).toEqual({reducer1: {test1: 'dispatch data'}});
+        done()
+    })
+    dispatch(action1())
 })
